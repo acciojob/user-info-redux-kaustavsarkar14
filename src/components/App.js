0199efb51f545834b4d95ec4fd9463b2ada0b61f
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-12-01 11:13:30 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -5,24 +5,27 @@ import { useDispatch, useSelector } from "react-redux";
 import { updateInfo } from "../redux/actions/userActions";
 
 const App = () => {
-  const [newName , setNewName] = useState('')
-  const [newEmail , setNewEmail] = useState('')
-  const user = useSelector(state=>state)
+  const [newName, setNewName] = useState('')
+  const [newEmail, setNewEmail] = useState('')
+  const user = useSelector(state => state)
   const dispatch = useDispatch()
 
-  useEffect(()=>{
-    dispatch(updateInfo({name:newName, email:newEmail}))
-  },[newEmail, newName])
+  useEffect(() => {
+    dispatch(updateInfo({ name: newName, email: newEmail }))
+  }, [newEmail, newName])
   return (
     <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" value={newName} onChange={e=>setNewName(e.target.value)} />
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" value={newEmail} onChange={e=>setNewEmail(e.target.value)} />
-        {/* Do not remove the main div */}
-      <p>Current values in store:</p>
-      <p>Name - {user.name}</p>
-      <p>Email - {user.email}</p>
+      <h1>User Information</h1>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" value={newName} onChange={e => setNewName(e.target.value)} />
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" value={newEmail} onChange={e => setNewEmail(e.target.value)} />
+      {/* Do not remove the main div */}
+      <div className="output">
+        <p>Current values in store:</p>
+        <p>Name - {user.name}</p>
+        <p>Email - {user.email}</p>
+      </div>
     </div>
   )
 }
